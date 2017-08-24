991eceef28ee18da088d03463b7de822791e5569
Gpwner <xuzhengchuang@126.com>
2017-08-24 22:35:35 +0800

    新增递归合并方法

--- 合并两个有序链表/leetcode/Main.java
+++ 合并两个有序链表/leetcode/Main.java
@@ -34,36 +34,48 @@ public class Main {
 
     }
 
+    //    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        if (l1 == null && l2 == null)
+//            return null;
+//        if (l1 == null)
+//            return l2;
+//        if (l2 == null)
+//            return l1;
+//        ListNode preHead = new ListNode(-1);
+//        ListNode iter = preHead;
+//
+//
+//        while (l1 != null && l2 != null) {
+//            if (l1.val < l2.val) {
+//                iter.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                iter.next = l2;
+//                l2 = l2.next;
+//            }
+//            iter = iter.next;
+//        }
+//
+//
+//
+//        if (l1 == null) {
+//            iter.next = l2;
+//        } else if (l2 == null) {
+//            iter.next = l1;
+//        }
+//        return preHead.next;
+//    }
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null)
-            return null;
         if (l1 == null)
             return l2;
         if (l2 == null)
             return l1;
-        ListNode preHead = new ListNode(-1);
-        ListNode iter = preHead;
-
-
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                iter.next = l1;
-                l1 = l1.next;
-            } else {
-                iter.next = l2;
-                l2 = l2.next;
-            }
-            iter = iter.next;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-
-
-
-        if (l1 == null) {
-            iter.next = l2;
-        } else if (l2 == null) {
-            iter.next = l1;
-        }
-        return preHead.next;
     }
-
 }
